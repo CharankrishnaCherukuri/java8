4f2ae282c215f192bd456e410d880b682ccbe57e
CharankrishnaCherukuri <113109038+CharankrishnaCherukuri@users.noreply.github.com>
2022-09-08 17:50:26 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,14 +1,39 @@
-package com.bct.java8gfeautres.methodreferences;
+package com.bct.java8features.methodreferences;
+
+
+
 import java.util.Arrays;
+
+
+
+//Method reference to an instance method of an arbitrary object of a particular type.
+
+
+
 
 public class Example {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] StringArray= {"charan","ganguly","vupendra","srikar","konanki yugandhar"};
-		
-		Arrays.sort(StringArray,String::comparetoIgnorecase);
 
-	}
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //Create a Collection
+        String[] stringArray = {
+                "charan","krishna","ram","kumar","vupendra","srikar","rohit"
+                
+        };
+        /*
+         * Method reference to an instance method of an arbitrary object of a particular type
+         */
+        Arrays.sort(stringArray, String::compareToIgnoreCase);
+        for(String data:stringArray) {
+            System.out.println(data);
+        }
+
+
+
+   }
+
+
 
 }
