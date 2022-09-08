22b575dbd0384f9b0ec3da2855c9aa22ed092b17
CharankrishnaCherukuri <113109038+CharankrishnaCherukuri@users.noreply.github.com>
2022-09-08 21:23:21 +0530

    Add files via upload

--- package-info.java
+++ package-info.java
@@ -1,1 +1,39 @@
-package java8;
+package com.bct.javafeauters.functional.interfaces;
+
+
+
+
+/*
+* Changes of interface on default and abstract method.
+*/
+interface DefaultAndAbstract {
+    
+    //default method -- do consist of method body.
+    default void print() {
+        System.out.println("Another example of default method in the interface");
+        System.out.println("Interface is mainly develop to obtain 100% abstaract");
+        
+}
+    
+    //abstract method - do not consist of method body.
+    public void myMethod(String message);
+}
+
+
+
+public class ChangesoffunctionalInterface implements DefaultAndAbstract {
+    
+        
+        //implementing abstract method
+        @Override
+        public void myMethod(String message) {
+            System.out.println("Message is : " + message);
+        }
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ChangesoffunctionalInterface instance = new ChangesoffunctionalInterface ();
+         instance.print();
+    }
